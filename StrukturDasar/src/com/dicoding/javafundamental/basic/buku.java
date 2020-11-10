0a4a460d3358495c4f9fe1cf1717a48cf903ea6c
ayufebriani <ayufebriani879@gmail.com>
2020-11-10 22:48:12 +0700

    publish file index

---
+++ StrukturDasar/src/com/dicoding/javafundamental/basic/buku.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.basic;
+
+/**
+ *
+ * @author ASUS
+ */
+public class buku {
+    public static void jumlahHalaman(){
+        System.out.println("Halaman 10");
+    }
+    } 
